5faabd65a9a2f8db6ce23683f39ebc0838300eb3
Hristo Genev <harrygg@gmail.com>
2016-06-15 16:48:22 +0300

    Fix for the new SEF urls

--- plugin.video.gospodari/addon.py
+++ plugin.video.gospodari/addon.py
@@ -1,52 +1,41 @@
 ﻿# -*- coding: utf-8 -*-
 import re, sys, urllib, os.path
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
-
-from resources.lib.helper import Helper
+from resources.lib.helper import Mode, Helper
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
 	
 _addon = xbmcaddon.Addon(id='plugin.video.gospodari')
-
-class Mode:
-    Categories = 0
-    Subcategories = 1
-    Videos = 2
-    Play = 3
-    FullShows = 4
-
 helper = Helper()
    
 def CATEGORIES():
-	addDir('Гафове', 0)
-	addDir('Репоражи', 1)
-	addDir('Скечове', 2)
-	addDir('Класации', 3)
-	addDir('Топ 20 най-гледаните днес', -1, Mode.Videos)
-	addDir('Топ 20 най-гледаните тази седмица', -2, Mode.Videos)
-	addDir('Топ 20 най-гледаните тази месец', -3, Mode.Videos)
-	addDir('Зоополиция', -4, Mode.Videos)
-	addDir('Цели предавания', -5)
+	addDir('Гафове', 1)
+	addDir('Репоражи', 2)
+	addDir('Скечове', 3)
+	addDir('Класации', 4)
+	addDir('Топ 20', 6)
+	addDir('Зоополиция', 7, Mode.Videos, 'http://gospodari.com/mobile/zoopolice')
+	addDir('Цели предавания', 0)
 	
 def SUBCATEGORIES(cat_id):
-	if cat_id == -5: #if we are looking for full seasons
+	if cat_id == 0: #if we are looking for full seasons
 		seasons = helper.get_seasons()
 		for s in seasons:
 			addDir(s['title'].encode('utf-8'), s['id'], Mode.FullShows)		
 	else:
 		data = helper.get_categories(cat_id)
-		for s in data['categories']:
-			addDir(s['name'].encode('utf-8'), s['id'], Mode.Videos)
+		for s in data:
+			addDir(s.get_text().encode('utf-8'), cat_id, Mode.Videos, s['href'])
 	
-def VIDEOS(cat_id, page):
-	videos = helper.get_videos(cat_id, page)
+def VIDEOS(cat_id, url, page):
+	videos = helper.get_videos(cat_id, url, page)
 	for v in videos:
-		addLink(v['title'].encode('utf-8'), v['id'], v['icon'])
+		addLink(v['title'].encode('utf-8'), v['url'], v['icon'])
 	
 	if helper.has_more_videos:
 		page = page + 1
-		addDir('Следваща страница >>>', cat_id, Mode.Videos, page)
+		addDir('Следваща страница >>>', helper.cat_id, Mode.Videos, '', page)
 	
 def FULLSHOWS(show_id, page):
 	videos = helper.get_full_shows(show_id, page)
@@ -56,17 +45,17 @@ def FULLSHOWS(show_id, page):
 	
 	if helper.has_more_videos:
 		page = page + 1
-		addDir('Следваща страница >>>', show_id, Mode.FullShows, page)
+		addDir('Следваща страница >>>', show_id, Mode.FullShows, '', page)
 	
-def PLAY(id, name, img, isFullShow):
-	url = helper.get_video_stream(id, isFullShow)
-	xbmc.log('Gospodari | play() | Will try to play item: ' + url)
-	li = xbmcgui.ListItem(iconImage = img, thumbnailImage = img, path =  url)
-	li.setInfo('video', { 'title': name })
-	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = url))
+def PLAY(url, name, img, isFullShow):
+	video = helper.get_video_info(url, isFullShow)
+	xbmc.log('Gospodari | play() | Will try to play item: ' + video['url'])
+	li = xbmcgui.ListItem(iconImage = img, thumbnailImage = img, path = video['url'])
+	li.setInfo('video', { 'title': name + ' ' + video['date'] })
+	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = video['url']))
 
-def addLink(name, id, img, isfullshow = False):
-	u = sys.argv[0] + "?id=" + str(id) + "&mode=3&name=" + urllib.quote_plus(name) + "&isfullshow=" + str(isfullshow)
+def addLink(name, url, img, isfullshow = False):
+	u = "%s?url=%s&mode=3&name=%s&isfullshow=%s" % (sys.argv[0], url, urllib.quote_plus(name), isfullshow)
 	ok = True
 	liz = xbmcgui.ListItem(name, iconImage = img, thumbnailImage = img)
 	liz.setInfo( type = "Video", infoLabels = { "Title": name } )
@@ -74,8 +63,8 @@ def addLink(name, id, img, isfullshow = False):
 	ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = liz, isFolder = False)
 	return ok
 
-def addDir(name, id, mode = Mode.Subcategories, page = 1):
-	u = sys.argv[0] + "?name=" + urllib.quote_plus(name) + "&mode=" + str(mode) + "&id=" + str(id) + "&page=" + str(page)
+def addDir(name, id, mode = Mode.Subcategories, url = '', page = 1):
+	u = "%s?name=%s&mode=%s&id=%s&page=%s&url=%s" % (sys.argv[0], urllib.quote_plus(name), mode, id, page, urllib.quote_plus(url))
 	ok = True
 	liz = xbmcgui.ListItem(name, iconImage = icon, thumbnailImage = icon)
 	liz.setInfo( type = "Video", infoLabels = { "Title": name } )
@@ -101,33 +90,26 @@ def get_params():
 
 params = get_params()
 
-id = None
 try: id = int(params["id"])
-except: pass
+except: id = None
 
-name = None
 try: name = urllib.unquote_plus(params["name"])
-except: pass
+except: name = None
 
-url = None
 try: url = urllib.unquote_plus(params["url"])
-except: pass
+except: url = None
 
-icon = xbmc.translatePath(os.path.join(_addon.getAddonInfo('path'), "icon.png"))
 try: icon = urllib.unquote_plus(params["icon"])
-except: pass
+except: icon = xbmc.translatePath(os.path.join(_addon.getAddonInfo('path'), "icon.png"))
 
-mode = None
 try: mode = int(params["mode"])
-except: pass
+except: mode = None
 
-page = 1
 try: page = int(params["page"])
-except: pass
+except: page = 1
 
-isfullshow = False
 try: isfullshow = params["isfullshow"] == 'True'
-except: pass
+except: isfullshow = False
 
 if mode == None:
 	CATEGORIES()
@@ -136,10 +118,10 @@ elif mode == Mode.Subcategories:
 	SUBCATEGORIES(id)
 
 elif mode == Mode.Videos:
-	VIDEOS(id, page)
+	VIDEOS(id, url, page)
 
 elif mode == Mode.Play:
-	PLAY(id, name, icon, isfullshow)
+	PLAY(url, name, icon, isfullshow)
 
 elif mode == Mode.FullShows:
 	FULLSHOWS(id, page)
--- plugin.video.gospodari/resources/lib/helper.py
+++ plugin.video.gospodari/resources/lib/helper.py
@@ -1,217 +1,186 @@
-import xbmc, os.path, json, urllib, urllib2, re, base64
+import sys, xbmc, os.path, json, urllib, urllib2, re, base64
+from bs4 import BeautifulSoup
 
+class Mode:
+  Categories = 0
+  Subcategories = 1
+  Videos = 2
+  Play = 3
+  FullShows = 4
+
+				
 class Helper():
-    has_more_videos = False
-    host = base64.b64decode('aHR0cDovL2dvc3BvZGFyaS5jb20v')
-    
-    def get_categories(self, id):
-        cats = []
-        #if user wants to watch seasons, get them from REST API:
-        if id == -4:
-            cats = self.get_seasons()
-        else:
-            #Otherwise get them from file
-            try:
-                filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'categories.json')
-                with open(filename) as data_file: 
-                    data = json.load(data_file)
-                cats = data["categories"][id]
-            except Exception, er:
-                xbmc.log("Gospodari | get_categories(" + str(id) + ") | Error: " + str(er))
-            
-        return cats
-    
-    def get_seasons(self):
-        seasons = []
-        try:
-            url = 'http://playapi.mtgx.tv/v3/seasons/videolist?format=4694'
-            res = Request(url)
-            data = json.loads(res)
-            seasons = data['_embedded']['seasons']
-        except Exception, er:
-            xbmc.log("Gospodari | Error:" + str(er))
-        return seasons
-    
-    def get_full_shows(self, id, page):
-        videos = []
-        try:
-            url = 'http://playapi.mtgx.tv/v3/videos?season=%s&page=%s&order=-visible_from' % (str(id), str(page))
-            res = Request(url)
-            data = json.loads(res)
-            videos = data['_embedded']['videos']
-            
-            #check if there are more videos
-            if page != data['count']['total_pages']:
-                self.has_more_videos = True
-            
-        except Exception, er:
-            xbmc.log("Gospodari | Error:" + str(er))
-        return videos
-    
-    def get_videos(self, id, page):
-        if id == 4:
-            return self.get_season_videos(id)
-        if page == 1:
-            response = Request(self.get_url(id))
-            if id < 0 and id > -4: #top20 videos
-                videos = self.get_top20_videos(response)
-            elif id == -4:
-                videos = self.get_zoo_videos(response)
-            else:
-                videos = self.extract_videos(response)
-        else:
-            if id == -4:
-                response = self.load_more_zoo_videos(page)
-                videos = self.get_zoo_videos(response, True)
-            else:
-                response = self.load_more_videos(id, page)
-                videos = self.extract_videos(response, True)
-        return videos
-    
-    def get_url(self, id):
-        
-        if id > 0:
-            u = self.host + 'mobile/index/' + str(id)
-        elif id == -1: #top20-dnes
-            u = self.host + 'mobile/top20-dnes'
-        elif id == -2: #top20
-            u = self.host + 'mobile/sedmichen-top'
-        elif id == -3: #top20
-            u = self.host + 'mobile/mesechen-top'
-        elif id == -4: #top20
-            u = self.host + 'mobile/zoopolice'
-        return u
-        
-    def load_more_videos(self, id, page):
-        url = base64.b64decode('aHR0cDovL2dvc3BvZGFyaS5jb20vbW9iaWxlL2xvYWRfbW9yZQ==')
-        post_data = 'cat_id=%s&page=%s&order=1' % (id, page)
-        return Request(url, post_data)
-        
-    def load_more_zoo_videos(self, page):
-        url = self.host + 'mobile/load_more_zoovideos'
-        post_data = 'page=%s&type=1' % page
-        return Request(url, post_data)
-    
-    def extract_videos(self, response, load_more = False):
-        videos = []
-        html = response
-        try:
-            #set has_more
-            matches = re.compile('(onclick[=\'"]+loadMore)').findall(response)
-            self.has_more_videos = True if len(matches) > 0 else False
-            
-            if load_more == False:
-                body = re.compile('class="scroll_down"(.*)load_ajax_up', re.DOTALL).findall(response)
-                html = body[0] if len(body) > 0 else ''
-                
-            xbmc.log("HTML: " + html)
-            regex = 'gospodari\.com/mobile/view/([0-9]+)'
-            ids = re.compile(regex).findall(html)
-            icons_and_titles = re.compile('i\.gospodari\.com/(.+?)\.jpg".*alt="(.*?)"').findall(html)
-            if len(ids) == len(icons_and_titles):
-              import HTMLParser
-              for i in range(0, len(ids)):
-                  video = {}
-                  video['id'] = ids[i]
-                  video['icon'] = 'http://i.gospodari.com/%s.jpg' % icons_and_titles[i][0]
-                  h = HTMLParser.HTMLParser()
-                  video['title'] = h.unescape(icons_and_titles[i][1])
-                  videos.append(video) 
-        except Exception, er:
-            xbmc.log("Gospodari | extract_videos() " + str(er))
-        return videos
-    
-    def get_top20_videos(self, html):
-        videos = []
-        regex = 'class="[a-zA-Z0-9\s]+big-video-holder(.*?)</div'
-        divs = re.compile(regex, re.DOTALL).findall(html)
-        if len(divs) > 0:
-            for i in range (0, len(divs)):
-                ids = re.compile('video([0-9]+).html').findall(divs[i])
-                icons_titles = re.compile('img.*src="(.*?)".+alt="(.*?)"').findall(divs[i])
-                #titles = re.compile('title=["\s\']+(.+?)["\s\']+').findall(divs[i])
-                if ids > 0 and icons_titles > 0:
-                    video = {}
-                    video['id'] = ids[0]
-                    video['icon'] = icons_titles[0][0]
-                    video['title'] = icons_titles[0][1]
-                    videos.append(video)
-        else: 
-            xbmc.log("Unable to find matching regex for rule: " + regex)
-            xbmc.log("Html: " + html)
-        return videos        
-    
-    def get_zoo_videos(self, html, loading_more = False):
-        videos = []
-        if loading_more == False:
-            regex = 'about-box-icon2(.*)load_more_wrap'
-            divs = re.compile(regex, re.DOTALL).findall(html)
-            source = divs[0]
-        else:
-            source = html
-            
-        #set has_more
-        matches = re.compile('(onclick[=\'"]+loadMore)').findall(html)
-        self.has_more_videos = True if len(matches) > 0 else False
-        
-        ids = re.compile('view/([0-9]+?)"').findall(source)
-        icons_titles = re.compile('img.*src="(.+?)".+alt="(.+?)"').findall(source)
-        if len(ids) > 0:
-            for i in range (0, len(ids)):
-                video = {}
-                video['id'] = ids[i]
-                video['icon'] = icons_titles[i][0]
-                video['title'] = icons_titles[i][1]
-                videos.append(video)
-        else: 
-            xbmc.log("Html: " + html)
-        return videos    
-        
-        
-    def get_video_stream(self, id, isFullShow):
-        stream = ''
-        try:
-            if isFullShow:
-                url = 'http://playapi.mtgx.tv/v3/videos/stream/' + str(id) 
-                res = Request(url)
-                data = json.loads(res)
-                medium = data['streams']['medium']
-                high = data['streams']['high']
-                hls = data['streams']['hls']
-                
-                if high != None:
-                    stream = high
-                elif medium != 'video://vod/':
-                    if 'stream.novatv.bg' in medium:
-                        stream = medium.replace('rtmp://stream.novatv.bg/mediacache/mp4:http/', 'http://videobg.novatv.bg/')
-                    elif 'host.bg' in medium:
-                        stream = medium.replace('rtmp://stream.by.host.bg/mediacache/flv:http/', 'http://videobg.novatv.bg/')
-                elif hls != None:
-                    stream = hls
-            else:
-                url = 'http://gospodari.com/mobile/view/' + str(id)
-                html = Request(url)
-                #file:"http://v.gospodari.com/f/videos/80/d93e8b96e484babd7ad35bee7e09847f.mp4"
-                url = re.compile('file[:"\']+(.*?\.[a-zA-Z0-9]+)["\'\s]{0,1}\}').findall(html)
-                if len(url) > 0:
-                    stream = url[0]
-        except Exception, er:
-            xbmc.log("Error in get_video_stream(id = %s): " % id + str(er))
-        
-        return stream
-    
+		has_more_videos = False
+		host = base64.b64decode('aHR0cDovL2dvc3BvZGFyaS5jb20v')
+		
+		def get_categories(self, id):
+			cats = []
+			#if user wants to watch seasons, get them from REST API:
+			if id == -4:
+				cats = self.get_seasons()
+			else: #Otherwise get them from mobile page
+				try:
+					#filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'categories.json')
+					res = Request('http://gospodari.com/mobile')
+					soup = BeautifulSoup(res, 'html5lib')
+					submenu = soup.find('div', class_='num_%s' % id)
+					return submenu.find_all('a')
+				except Exception, er:
+					xbmc.log("Gospodari | get_categories(" + str(id) + ") | Error: " + str(er))
+			return cats
+		
+		def get_seasons(self):
+			seasons = []
+			try:
+				url = base64.b64decode('aHR0cDovL3BsYXlhcGkubXRneC50di92My9zZWFzb25zL3ZpZGVvbGlzdD9mb3JtYXQ9NDY5NA==')
+				res = Request(url)
+				data = json.loads(res)
+				seasons = data['_embedded']['seasons']
+			except Exception, er:
+				xbmc.log("Gospodari | Error:" + str(er))
+			return seasons
+		
+		def get_full_shows(self, id, page):
+			videos = []
+			try:
+				url = base64.b64decode('aHR0cDovL3BsYXlhcGkubXRneC50di92My92aWRlb3M/c2Vhc29uPSVzJnBhZ2U9JXMmb3JkZXI9LXZpc2libGVfZnJvbQ==') % (id, page)
+				res = Request(url)
+				data = json.loads(res)
+				videos = data['_embedded']['videos']
+				
+				#check if there are more videos
+				if page != data['count']['total_pages']:
+					self.has_more_videos = True
+				
+			except Exception, er:
+				xbmc.log("Gospodari | Error:" + str(er))
+			return videos
+		
+		def get_videos(self, id, url, page):
+			if page == 1:
+				response = Request(urllib.unquote_plus(url))
+				if id == 6: #top20 videos
+					videos = self.get_top20_videos(response)
+				else:
+					videos = self.extract_videos(response)
+			else:
+				if id == 7: #Zoo videos are loaded from a different source
+					response = self.load_more_zoo_videos(page)
+				else:
+					response = self.load_more_videos(id, page)
+				videos = self.extract_videos(response, True)
+			return videos
+				
+		def load_more_videos(self, id, page):
+				url = base64.b64decode('aHR0cDovL2dvc3BvZGFyaS5jb20vbW9iaWxlL2xvYWRfbW9yZQ==')
+				post_data = 'cat_id=%s&page=%s&order=1' % (id, page)
+				return Request(url, post_data)
+				
+		def load_more_zoo_videos(self, page):
+				url = self.host + 'mobile/load_more_zoovideos'
+				post_data = 'page=%s&type=1' % page
+				return Request(url, post_data)
+		
+		def extract_videos(self, response, load_more = False):
+			videos = []
+			html = response
+			try:
+				#set has_more
+				matches = re.compile('(onclick[=\'"]+loadMore.*?)\((.+?)\)').findall(response)
+				self.has_more_videos = True if len(matches) > 0 else False
+				try :
+					params = matches[0][1].split(',')
+					if len(params) > 2: #If we are in the zoopolice page there will be 2 params only
+						try: self.cat_id = params[0]
+						except: self.cat_id = 0
+					else:
+						self.cat_id = 7
+				except: pass
+						
+				soup = BeautifulSoup(html.decode('utf-8', 'ignore'), 'html5lib')
+				items = soup.find_all('a', {'class':['video_box_wrap', 'article_box']})
+				xbmc.log("Gospodari found %s items: " % len(items))
+				if len(items) == 0:
+					xbmc.log("HTML: " + html)
+				
+				for i in range(0, len(items)):
+					video = {}
+					url = items[i]['href'].encode('utf-8')
+					video['url'] = urllib.quote_plus(url)
+					video['icon'] = items[i].img['src']
+					video['title'] = items[i]['title'].encode('utf-8')
+					videos.append(video)
+			except Exception, er:
+				xbmc.log("Gospodari | extract_videos() %s" % er)
+			return videos
+		
+		def get_top20_videos(self, html):
+			videos = []
+			soup = BeautifulSoup(html.decode('utf-8', 'ignore'), 'html5lib')
+			items = soup.find_all('div', class_='big-video-holder')
+			#regex = 'class="[a-zA-Z0-9\s]+big-video-holder(.*?)</div'
+			#divs = re.compile(regex, re.DOTALL).findall(html)
+			if len(items) > 0:
+				for i in range (0, len(items)):
+					video = {}
+					a = items[i].find('a')
+					video['url'] = a['href']
+					video['icon'] = a.img['src']
+					video['title'] = a['title'].encode('utf-8')
+					videos.append(video)
+			else: 
+				xbmc.log("Unable to find matching regex for rule: " + regex)
+				xbmc.log("Html: " + html)
+			return videos				
+				
+		def get_video_info(self, url, isFullShow):
+			stream = {}
+			try:
+				if isFullShow:
+					url = 'http://playapi.mtgx.tv/v3/videos/stream/%s' % url 
+					res = Request(url)
+					data = json.loads(res)
+					medium = data['streams']['medium']
+					high = data['streams']['high']
+					hls = data['streams']['hls']
+					
+					if high != None:
+						stream['url'] = high
+					elif medium != 'video://vod/':
+						if 'stream.novatv.bg' in medium:
+							stream['url'] = medium.replace('rtmp://stream.novatv.bg/mediacache/mp4:http/', 'http://videobg.novatv.bg/')
+						elif 'host.bg' in medium:
+							stream['url'] = medium.replace('rtmp://stream.by.host.bg/mediacache/flv:http/', 'http://videobg.novatv.bg/')
+					elif hls != None:
+						stream['url'] = hls
+					stream['date'] = ''
+				else:
+					if not url.startswith('http'): 
+						url = 'http://gospodari.com/%s' % url
+					html = Request(url)
+					#soup = BeautifulSoup(html.decode('utf-8', 'ignore'), 'html5lib')
+					#file:"http://v.gospodari.com/f/videos/80/d93e8b96e484babd7ad35bee7e09847f.mp4"
+					stream['url'] = re.compile('file[:"\'\s]+(http.+?mp4)').findall(html)[0]
+					try:
+						stream['date'] = re.compile('ubuntubold">(.+?)<').findall(html)[0]
+					except: stream['date'] = ''
+			except Exception, er:
+				xbmc.log("Error in get_video_stream(id = %s): " % id + str(er))
+				
+			return stream
+		
 def Request(url, data = ''):
-    xbmc.log("Gospodari | Sending request to: " + url)
-    try:
-        req = urllib2.Request(url) if data == '' else urllib2.Request(url, data) 
-        req.add_header('User-Agent', MUA)
-        res = urllib2.urlopen(req)
-        response = res.read()
-        res.close()
-        #xbmc.log("Response: " + response)
-        return response
-    except Exception, er:
-        xbmc.log("Gospodari | Request error: " + str(er))
-        return ""
+		xbmc.log("Gospodari | Sending request to: " + url)
+		try:
+				req = urllib2.Request(url) if data == '' else urllib2.Request(url, data) 
+				req.add_header('User-Agent', MUA)
+				res = urllib2.urlopen(req)
+				response = res.read()
+				res.close()
+				#xbmc.log("Response: " + response)
+				return response
+		except Exception, er:
+				xbmc.log("Gospodari | Request error: " + str(er))
+				return ""
 
 MUA = 'Mozilla/5.0 (Linux; Android 5.0.2; bg-bg; SAMSUNG GT-I9195 Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Version/1.0 Chrome/18.0.1025.308 Mobile Safari/535.19' 
